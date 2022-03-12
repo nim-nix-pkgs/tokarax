15a1891e76d55d18ae5c00da0e264b1bad73acc3
hugosenari <hugosenari@gmail.com>
2022-03-12 03:40:45 -0300

    chore: re index tokarax

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Converts HTML to Karax representation'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."tokarax-master".url = "path:./master";
+  inputs."tokarax-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tokarax-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
